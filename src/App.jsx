de807847db52a7455aa11cf719ca5047968aae88
Steve Maranga <stevemaranga09@gmail.com>
2025-12-27 02:58:57 +0300

    steve

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+      
+    </>
+  )
+}
+
+export default App
