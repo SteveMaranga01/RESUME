905e6759d57d49eac39edc48691b951bc948fa5e
Steve Maranga <stevemaranga09@gmail.com>
2025-12-27 05:10:24 +0300

    steve

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,41 @@
+import React, { useState } from 'react';
+
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [activeLink, setActiveLink] = useState('#home');
+    const Navlinks = [
+        {href: '#home', label: 'Home'},
+        {href: '#about', label: 'About Us'},
+        {href: '#services', label: 'Our Services'},
+        {href: '#testimonials', label: 'Testimonials'},
+    ]
     return (
-        <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
-            <div className="w-full container mx-auto flex items-center justify-between px-4 sm sm:px-6 lg:px-8 md:h-20 h16 "></div>
+        <nav className='fixed top-0 left-0 right-0 bg-white/90 
+        backdrop-blur-sm z-50 border-b border-gray-100 shadow-sm'>
+            <div className="w-full container mx-auto flex items-center 
+            justify-between px-4 sm sm:px-6 lg:px-8 md:h-20 h16 ">
+
+                {/*logo*/}
+
+                <div className="flex items-center gap-1 cursor-pointer">
+                    <div className="w-4 h-4 bg-blue-600 rounded-full opacity-75 
+                    hover:opacity-100 transtion-opacity"></div>
+                    <div className="w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100
+                     hover:opacity-75 transtion-opacity"></div>
+                </div>
+
+                {/*desktop navitems*/}
+
+                <div>
+                    {Navlinks.map((link, index) => (
+                        <a href={link.href} key={index}
+                        className={"text-sm font-medium relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 hover:after:w-full after:bg-blue-600 after:transition-all"}
+                        >{link.label}</a>
+                    ))}
+                </div>
+                {/*get in touch btn*/}
+
+            </div>
       </nav>
     )
 }
