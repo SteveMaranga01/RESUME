07cb367ecd12c1974fa7830d414ff654e9fe3b17
SteveMaranga01 <stevemaranga09@gmail.com>
2025-12-31 16:34:44 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import './App.css'
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import CompanyLogo from './components/CompanyLogo';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Navbar/>
-      <Hero/>
+    <main className='relative min-h-screen overflow-x-hidden'>
+      <div className='absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500 from-indigo-500/20 to-pink-500 opacity-20 rounded-full blur-[80px] -z-10'></div>
+      <div className='overflow-hidden '>
+        <Navbar/>
+        <Hero/>
+        <CompanyLogo/>
+
+      </div>
       
-    </>
+    </main>
   )
 }
 
